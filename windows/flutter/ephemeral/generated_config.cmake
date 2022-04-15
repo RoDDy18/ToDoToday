5e1d98a8a854e3da47a12fa9154077f242698ae7
Emmanuel Oni <damilolaoni2004@gmail.com>
2022-04-15 18:06:51 +0100

    upload
    uploading files

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\CSE\\flutter SDK" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ADENIKEONI\\Documents\\LINCOLN UNIVERSITY COLLEGE\\3rd SEMESTER\\CSE 231\\Flutter Projects\\Flutter App\\ui_assignment" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\CSE\\flutter SDK"
+  "PROJECT_DIR=C:\\Users\\ADENIKEONI\\Documents\\LINCOLN UNIVERSITY COLLEGE\\3rd SEMESTER\\CSE 231\\Flutter Projects\\Flutter App\\ui_assignment"
+  "FLUTTER_ROOT=C:\\CSE\\flutter SDK"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ADENIKEONI\\Documents\\LINCOLN UNIVERSITY COLLEGE\\3rd SEMESTER\\CSE 231\\Flutter Projects\\Flutter App\\ui_assignment\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ADENIKEONI\\Documents\\LINCOLN UNIVERSITY COLLEGE\\3rd SEMESTER\\CSE 231\\Flutter Projects\\Flutter App\\ui_assignment"
+  "FLUTTER_TARGET=C:\\Users\\ADENIKEONI\\Documents\\LINCOLN UNIVERSITY COLLEGE\\3rd SEMESTER\\CSE 231\\Flutter Projects\\Flutter App\\ui_assignment\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ADENIKEONI\\Documents\\LINCOLN UNIVERSITY COLLEGE\\3rd SEMESTER\\CSE 231\\Flutter Projects\\Flutter App\\ui_assignment\\.dart_tool\\package_config.json"
+)
